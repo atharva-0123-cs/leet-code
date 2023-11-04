cf4d10cbff26c1a4567fd5e7a049d50baa843de9
atharva-0123-cs <atharva123king@gmail.com>
2023-11-04 11:28:14 +0530

    Unsupported Server Component type: Module

--- app/problems/[pid]/page.jsx
+++ app/problems/[pid]/page.jsx
@@ -2,15 +2,22 @@ import TopBar from "@/app/components/TopBar/TopBar";
 import Workspace from "@/app/components/Workspace/Workspace";
 import { problems } from "@/app/utils/Examples/page";
 
-const ProblemPages = ({params}) => {
+
+async function getProblems(pid) {
+  const problem = problems[pid];
+  problem.handlerFunction = problem.handlerFunction.toString();
+
+  return problem
+}
+
+const ProblemPages = async ({params}) => {
 
   console.log(params);
   const {pid} = params;
-  // console.log("pid == ",pid);
+  console.log("pid == ",pid);
 
-  const problem = problems[pid];
-  problem.handlerFunction = problem.handlerFunction.toString();
-  // console.log(problem);
+  const problem = await getProjects(pid)
+  console.log(problem);
 
     return (
         <>
@@ -24,14 +31,16 @@ const ProblemPages = ({params}) => {
 // Return a list of `params` to populate the [pid] dynamic segment
 export async function generateStaticParams() {
     const paths = Object.keys(problems);
-  //   if(paths)
-  //     console.log(paths);
-  //  else 
-  //     console.log("paths Not Found");
+    if(paths)
+      console.log(paths);
+   else 
+      console.log("paths Not Found");
 
     return paths.map((path) => ({
       params: {pid : path},
     }));
   }
+
+ 
 
 export default ProblemPages;
